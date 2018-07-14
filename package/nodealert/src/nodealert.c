575a14099e60c4d3de716581d0533d0b6b5d9873
Christof Schulze <christof.schulze@gmx.net>
2018-07-15 01:57:23 +0200

    fixup! nodealert: add package

--- package/nodealert/src/nodealert.c
+++ package/nodealert/src/nodealert.c
@@ -1,13 +1,36 @@
 #include <json-c/json.h>
 #include <respondd.h>
 #include <uci.h>
+#include <string.h>
 
 #include "alert.h"
 
 int get_nodealert_isactive(void) {
-	const char *c = uci_lookup_option_string(ctx, "settings", "active");
+	const char *c = NULL;
+
+	ctx = uci_alloc_context();
+	if (!ctx)
+		goto error;
+	ctx->flags &= ~UCI_FLAG_STRICT;
+
+	if (uci_load(ctx, "nodealert", &p))
+		goto error;
+
+	c = uci_lookup_option_string(ctx, "settings", "active");
 	printf("uci looked up: %s\n", c);
+
+	if (ctx) {
+		if (p)
+			uci_unload(ctx, p);
+		uci_free_context(ctx);
+	}
+
+end:
+	if (! c)
+		return -1;
 	return ! strncmp(c, "true", 4);
+error:
+	goto end;
 }
 
 struct json_object *alertme(void) {
@@ -18,32 +41,10 @@ struct json_object *alertme(void) {
 	if (!ret)
 		return NULL;
 
-	ctx = uci_alloc_context();
-	if (!ctx)
-		goto error;
-	ctx->flags &= ~UCI_FLAG_STRICT;
-
-	if (uci_load(ctx, "nodealert", &p))
-		goto error;
-
 	bool isactive_nodealert = get_nodealert_isactive();
 	struct json_object *j_isactive = json_object_new_boolean(isactive_nodealert);
 	json_object_object_add(ret, "nodealert_active", j_isactive);
-
-	return ret;
-
 end:
-	if (ctx) {
-		if (p)
-			uci_unload(ctx, p);
-		uci_free_context(ctx);
-	}
 	return ret;
-error:
-	if(ret)
-		free(ret);
-	ret = NULL;
-	goto end;
 }
-
 
